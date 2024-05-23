eb000fcaf86f64b0041211faf093ce036f510a0a
jh <selfown711@gmail.com>
2024-05-23 15:28:36 +0900

    Init : Create Management App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gray-background">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Let's develop management system!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
